2231eeb43354af48b82819ca17f8d30d46a340a0
amachkel <amachkel@gmail.com>
2022-02-07 11:35:50 -0500

    Added search results to object. Object inclues current and forecast data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-// let initSearchButtons = () => {
-//     let localStorageCities = localStorage.getItem("SearchedCities");
-//   let cities = [];
+let initSearchButtons = () => {
+  let localStorageCities = localStorage.getItem("SearchedCities");
+  let cities = [];
 
-//   if (localStorageCities != null) {
-//     cities = JSON.parse(localStorageCities);
-//   }
-//   for(let i = 0; i < cities.length; i++){
-//       renderSearchBtn(cities[i]);
-//   }
-// }
+  if (localStorageCities != null) {
+    cities = JSON.parse(localStorageCities);
+  }
+  for (let i = 0; i < cities.length; i++) {
+    renderSearchBtn(cities[i]);
+  }
+};
 
 let eventHandler = () => {
   let searchButtonEl = document.getElementById("searchBtn");
@@ -48,62 +48,65 @@ let renderSearchBtn = (cityName) => {
 };
 
 let getCoordinates = (cityName) => {
-  let currentWeatherEl = document.getElementById("current-weather");
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=585ba3d2e5d78c9afea8cfd73fcf8a69`
   )
     .then((response) => response.json())
     .then((data) => {
-      if (!data) {
-        console.log("No results found");
+      //Needs to be fixed
+      if (data.cod != null && data.cod == "404") {
         currentWeatherEl.innerHTML =
           "<h3>No results found, try another search.</h3>";
         return;
       }
-      console.log(data);
-      let lon = data.city.coord.lon;
-      let lat = data.city.coord.lat;
-      console.log(lon, lat);
-      getWeatherResults(lon, lat);
+      let resultsObj = {};
+      resultsObj.cityName = cityName;
+      resultsObj.lon = data.city.coord.lon;
+      resultsObj.lat = data.city.coord.lat;
+      getWeatherResults(resultsObj);
     });
 };
 
-let getWeatherResults = (lon, lat) => {
+let getWeatherResults = (resultsObj) => {
+  let lat = resultsObj.lat;
+  let lon = resultsObj.lon;
   fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=585ba3d2e5d78c9afea8cfd73fcf8a69`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      let tempVal = data.current.temp;
-      let humidityVal = data.current.humidity;
-      let uviVal = data.current.uvi;
-      let windspeedVal = data.current.wind_speed;
-      let iconVal = data.current.weather[0].icon;
+      resultsObj.tempVal = data.current.temp;
+      resultsObj.humidityVal = data.current.humidity;
+      resultsObj.uvi = data.current.uvi;
+      resultsObj.windspeedVal = data.current.wind_speed;
+      resultsObj.iconVal = data.current.weather[0].icon;
       //icon returns as string. use this url to append to html:
       //http://openweathermap.org/img/wn/{icon code}@2x.png
-      console.log(tempVal, humidityVal, uviVal, windspeedVal, iconVal);
+      console.log(resultsObj);
 
-      getForecastResults(data);
+      getForecastResults(resultsObj, data);
     });
 };
 
-let getForecastResults = (data) => {
+let getForecastResults = (resultsObj, data) => {
+  resultsObj.forecastResults = [];
   for (let i = 1; i < 6; i++) {
-    let forecastTemp = data.daily[i].temp.day;
-    let forecastHumidity = data.daily[i].humidity;
-    let forecastUvi = data.daily[i].uvi;
-    let forecastWind = data.daily[i].wind_speed;
-    let forecastIcon = data.daily[i].weather[0].icon;
-    console.log(
-      forecastTemp,
-      forecastHumidity,
-      forecastUvi,
-      forecastWind,
-      forecastIcon
-    );
+    let forecast = {};
+    //new Date using unix value and converting to forecast date
+    forecast.date = new Date(data.daily[i].dt * 1000);
+    forecast.tempVal = data.daily[i].temp.day;
+    forecast.humidityVal = data.daily[i].humidity;
+    forecast.uvi = data.daily[i].uvi;
+    forecast.windspeedVal = data.daily[i].wind_speed;
+    forecast.iconVal = data.daily[i].weather[0].icon;
+    resultsObj.forecastResults.push(forecast);
   }
+  console.log(resultsObj);
 };
 
+let renderResults = (resultsObj) => {
+    
+}
 eventHandler();
-// initSearchButtons();
+initSearchButtons();
